c9a0ce056e85fb7d064d133880b00ec32618d0ee
Balogun Ademola <35502829+Demmynile@users.noreply.github.com>
2021-02-25 15:30:00 +0100

    Update settings.py

--- contactsapi/settings.py
+++ contactsapi/settings.py
@@ -99,7 +99,7 @@ WSGI_APPLICATION = 'contactsapi.wsgi.application'
 
 DATABASES = {
    'default': {
-        'ENGINE': 'django.db.backends.mysqlite',
+        'ENGINE': 'django.db.backends.mysqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
      
     }
